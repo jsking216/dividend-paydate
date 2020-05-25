537061963e898aef208f890291a3947ba9e3c1a2
jsking216 <jking@onecause.com>
2020-05-24 21:39:30 -0400

    add babel, write paydates to file

---
+++ getPaydates.js
@@ -0,0 +1,44 @@
+
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');
+
+async function getTextFromElements(elements) {
+    return Promise.all(elements.map(async (element) => {
+        const elementText = await element.getText();
+        return elementText;
+    }));
+}
+
+export async function getPaydates() {
+    
+    chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
+
+    var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+    await driver.get('https://www.nasdaq.com/market-activity/dividends');
+    try {
+        await driver.wait(webdriver.until.elementLocated(webdriver.By.className('evidon-banner-declinebutton')), 15000, 'waiting for cookie accept button');
+        await driver.findElement(webdriver.By.className('evidon-banner-declinebutton')).click();
+        await driver.findElement(webdriver.By.css('th.market-calendar-table__columnheader:nth-child(4) > button:nth-child(1) > span:nth-child(1)')).click();
+        const tickers = await driver.findElements(webdriver.By.css('[data-column="symbol"]'));
+        const exdiv = await driver.findElements(webdriver.By.css('[data-column="dividend_Ex_Date"]'));
+        const paydates = await driver.findElements(webdriver.By.css('[data-column="payment_Date"]'));
+        const tickerArray = await getTextFromElements(tickers);
+        const exdivArray = await getTextFromElements(exdiv);
+        const paydatesArray = await getTextFromElements(paydates);
+
+        // push everything into an array of objects representing the data for a single ticker
+        const allData = [];
+        for(let i = 0; i < tickerArray.length; i++) {
+            allData.push({ ticker: tickerArray[i], exdiv: exdivArray[i], paydate: paydatesArray[i] });
+            console.log(JSON.stringify(allData[i]));
+        }
+    }
+    catch (e) {
+        console.log(`encountered error: ${e}`);
+    }
+    await driver.quit();
+    return allData;
+}
--- index.js
+++ index.js
@@ -1,40 +1,31 @@
+import { getPaydates } from './getPaydates';
+import { readFileSync, writeFileSync }  from 'fs';
 
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const chromedriver = require('chromedriver');
+const datafile = 'persistentPaydates.json';
 
-chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
-
-async function getPaydates() {
-    var driver = new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities.chrome())
-    .build();
-    await driver.get('https://www.nasdaq.com/market-activity/dividends');
-    await driver.wait(webdriver.until.elementLocated(webdriver.By.className('evidon-banner-declinebutton')), 10000, 'waiting for cookie accept button');
-    await driver.findElement(webdriver.By.className('evidon-banner-declinebutton')).click();
-    await driver.findElement(webdriver.By.css('th.market-calendar-table__columnheader:nth-child(4) > button:nth-child(1) > span:nth-child(1)')).click();
-    const tickers = await driver.findElements(webdriver.By.css('[data-column="symbol"]'));
-    const exdiv = await driver.findElements(webdriver.By.css('[data-column="dividend_Ex_Date"]'));
-    const paydates = await driver.findElements(webdriver.By.css('[data-column="payment_Date"]'));
-    const tickerArray = await getTextFromElements(tickers);
-    const exdivArray = await getTextFromElements(exdiv);
-    const paydatesArray = await getTextFromElements(paydates);
-
-    // push everything into an array of objects representing the data for a single ticker
-    const allData = [];
-    for(let i = 0; i < tickerArray.length; i++) {
-        allData.push({ ticker: tickerArray[i], exdiv: exdivArray[i], paydate: paydatesArray[i] });
-        console.log(JSON.stringify(allData[i]));
-    }
-
-    await driver.quit();
+function readPaydates() {
+  const paydates = [];
+  const existing = JSON.parse(readFileSync(datafile, 'utf8'));
+  existing.map((elem) => {
+    paydates.push(elem);
+  });
+  return paydates;
 }
 
-async function getTextFromElements(elements) {
-    return Promise.all(elements.map(async (element) => {
-        const elementText = await element.getText();
-        return elementText;
-    }));
+function writePaydates(paydates) {
+  const asJson = JSON.stringify(paydates);
+  writeFileSync(datafile, asJson, 'utf8');
 }
 
-getPaydates();
+async function processPaydates() {
+  const allPaydates = readPaydates();
+  const newPaydates = await getPaydates();
+
+  newPaydates.map((elem) => {
+    allPaydates.push(elem);
+  });
+
+  writePaydates(allPaydates);
+}
+
+processPaydates();
