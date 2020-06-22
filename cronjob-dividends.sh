69321df78621ca102b7c68db382f791a9328227b
jsking216 <jking@onecause.com>
2020-06-22 08:58:36 -0400

    autoupdate chromedriver ver from script/env var

--- cronjob-dividends.sh
+++ cronjob-dividends.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export PATH="$PATH:"/usr/local/bin/
+CHROMEVER=$(curl "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$(Google\ Chrome --version | sed -E -n 's/^.* (([0-9]+\.){2}[0-9]+).*$/\1/p')")
 cd /Users/jking/personal/dividend-paydate
-./node_modules/.bin/babel-node index.js 
+CHROMEDRIVER_VERSION=$CHROMEVER ./node_modules/.bin/babel-node index.js 
